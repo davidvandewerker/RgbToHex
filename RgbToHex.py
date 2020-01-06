e1f49e7880794dd29dc2d8bf9ae55e9205a37a3e
davidvandewerker <56415208+davidvandewerker@users.noreply.github.com>
2020-01-05 22:30:18 -0700

    Update RgbToHex.py

--- RgbToHex.py
+++ RgbToHex.py
@@ -1,14 +1,40 @@
-from sys import argv
+import argparse
 from colors import rgb, hex
+from argparse import RawTextHelpFormatter
 
-script, red, green, blue = argv
+parser = argparse.ArgumentParser(
+    prog='RgbToHex.py',
+    description='convert rgb values to hexadecimal values',
+    epilog='related programs: HexToRgb.py',
+    formatter_class=RawTextHelpFormatter
+)
 
-red = int(red)
-green = int(green)
-blue = int(blue)
+parser.add_argument(
+    'red',
+    help='red color value (3 digit number [0-255])',
+    type=int
+)
+
+parser.add_argument(
+    'green',
+    help='green color value (3 digit number [0-255])',
+    type=int
+)
+
+parser.add_argument(
+    'blue',
+    help='blue color value (3 digit number [0-255])',
+    type=int
+)
+
+args = parser.parse_args()
+
+red = args.red
+green = args.green
+blue = args.blue
 
 rgb_color = str(rgb(red, green, blue))
 hex_color = str(rgb(red, green, blue).hex).upper()
 
-print('\n' + 'RGB Value: rgb(' + rgb_color + ')')
-print('Hex Value: #' + hex_color + '\n')
+print('\n' + 'rgb value:', '\n rgb(' + rgb_color + ')\n')
+print('hex value:', '\n #' + hex_color, '\n')
